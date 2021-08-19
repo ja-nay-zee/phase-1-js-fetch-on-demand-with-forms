72970a1aa56da61198b38d9a25c9bb0659f966f9
Yaneizy Castillo <yccastillo18@gmail.com>
2021-08-18 22:44:40 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+    //   console.log(event)
+
+    //   event.target.children[1];
+
+    const input = document.querySelector('input#searchByID');
+    // console.log(input.value);
+    
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(resp => resp.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summery;
+    // console.log(data);
+    })
+ }) 
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
